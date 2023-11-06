6bda74a8fb9f75256626148fbf1ad5fd98f54071
grin <kerstin.pfaffinger@tum.de>
2023-11-06 21:33:20 +0100

    basic game + question initializer tool. stop if out of questions

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,8 +1,15 @@
 extends Control
 
-var background_color: ColorRect
+@export_category("End")
+@export var end_text: String = "no more questions :("
+@export var end_color: Color = Color.BLACK
 
-var colors: Array = [
+
+@export_category("Questions")
+@export var questions: Array
+
+@export_category("Colors")
+@export var colors: Array = [
 	Color.RED, # make less poppy
 	Color.LAWN_GREEN,
 	Color.YELLOW, # maybe a bit too bright
@@ -12,16 +19,26 @@ var colors: Array = [
 	Color.ORANGE
 ]
 
+
+### COLOR
+var background_color: ColorRect
 var number_of_colors: int
-var last_index: int = -1
+var last_color_index: int = -1
+
+### TEXT
+var question_label: Label
+var number_of_questions: int
+var question_index: int = 0
 
 
 func _ready():
 	background_color = $BackgroundColor
 	number_of_colors = colors.size()
 	
+	question_label = $QuestionBox/QuestionLabel
+	number_of_questions = questions.size()
+	
 	next_question()
-
 
 
 func _input(event):
@@ -31,24 +48,39 @@ func _input(event):
 
 
 func next_question() -> void:
-	print("NEXT QUESTION")
 	next_text()
 	next_background_color()	
 
+
 func next_text() -> void:
-	pass
+	if question_index == number_of_questions:
+		stop_listening_for_input()
+		display_end_screen()
+		return
+		
+	question_label.text = questions[question_index]
+	question_index += 1
+
+
+func display_end_screen() -> void:
+	question_label.text = end_text
+	background_color.color = end_color
+	
+
+func stop_listening_for_input() -> void:
+	set_process_input(false)
 
 
 func next_background_color() -> void:
-	background_color.color = colors[get_random_index()]
+	background_color.color = colors[get_random_color_index()]
 
 
-func get_random_index() -> int:
+func get_random_color_index() -> int:
 	# randi_range param "to" is inclusive
 	var random_index: int = randi_range(0, number_of_colors - 1)
 	
 	# ensure different color
-	if random_index == last_index:
+	if random_index == last_color_index:
 		random_index = (random_index + 1) % number_of_colors
 	
 	return random_index
---
+++ scripts/question_initializer.gd
@@ -0,0 +1,37 @@
+@tool
+extends EditorScript
+
+var question_txt_path: String = "res://data/questions.txt"
+var questions_node_name: String = "QuestionsMain"
+var print_question_array: bool = true
+
+var questions: Array
+
+func get_questions() -> Array:
+	return questions
+
+
+func _run():
+	fill_array_from_file()
+	
+	if print_question_array:
+		print(questions)
+		
+	assign_questions_array_in_main()
+		
+
+func fill_array_from_file() -> void:
+	var questions_file = FileAccess.open(question_txt_path, FileAccess.READ)
+	while not questions_file.eof_reached():
+		var line: String = questions_file.get_line()
+		
+		if line.begins_with("#") or line.is_empty():
+			continue
+		
+		questions.append(str(line))
+
+
+func assign_questions_array_in_main() -> void:
+	# get scene returns the root node
+	var questions_node = get_scene()
+	questions_node.questions = questions
