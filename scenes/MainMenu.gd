ca90799b0c7f0b6b65070c85a5717a6bdae116f3
grin <kerstin.pfaffinger@tum.de>
2023-12-06 23:22:01 +0100

    refactor

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,18 +1,29 @@
 extends Control
 
-const questions_scene_path: String = "res://scenes/questions.tscn"
+### SCENE PATHS
+const questions_game_scene_path: String = "res://scenes/questions.tscn"
+const add_questions_scene_path: String = "res://scenes/questions.tscn"
+
+### OTHER
 var background: ColorRect
 var scene_changer: SceneChanger
 
+
+### INIT
 func _ready():
 	background = $BackgroundColor
 	scene_changer = SceneChanger.new()
 	
-
+	
+### EVENTS
 func _on_start_button_down():
-	scene_changer.change_scene(questions_scene_path, get_tree())
+	call_change_scene(questions_game_scene_path)
 
 
 func _on_add_button_down():
-	# TODO: show question enter panel
-	pass # Replace with function body.
+	call_change_scene(add_questions_scene_path)
+
+
+### HELPERS
+func call_change_scene(path: String) -> void:
+	scene_changer.change_scene(path, get_tree())
